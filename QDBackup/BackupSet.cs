d88a4236e5985c4b2df0a563f7da7f629ab418b4
Jake Bladt <jake@jakebladt.com>
2014-03-08 22:35:49 -0500

    filled in backup set

--- QDBackup/BackupSet.cs
+++ QDBackup/BackupSet.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace QDBackup
 {
-    public class BackupSet : List<BackupFileSource>
+    public class BackupSet : Dictionary<int, BackupFileSource>
     {
+        public string Name { get; protected set; }
+        public string TargetPath { get; protected set; }
+        public string SourcePath { get; protected set; }
+        public List<ExclusionRule> Exclusions { get; protected set; }
     }
 }
---
+++ QDBackup/ExclusionRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDBackup
+{
+    public class ExclusionRule
+    {
+        string Name { get; protected set; }
+        string Path { get; protected set; }
+    }
+}
