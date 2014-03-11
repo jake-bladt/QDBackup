4ef7ea493d5f1274fd34b344737f8602993fc2d8
Jake Bladt <jake@jakebladt.com>
2014-03-11 12:25:20 -0400

    interim

--- QDBackup/BackupSetList.cs
+++ QDBackup/BackupSetList.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using QDBackup.Mappers;
+
 namespace QDBackup
 {
     public class BackupSetList : Dictionary<string, BackupSet>
     {
+        protected IBackupSetListMapper _mapper;
+
+        public BackupSetList(IBackupSetListMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public bool Save()
+        {
+            return _mapper.Save(this);
+        }
+
     }
 }
