c7d3f28982454c9bb1cab0eb37e02512169ae442
Jake Bladt <jake@jakebladt.com>
2014-03-11 22:55:55 -0400

    save/load in XmlBSLMapper

--- QDBackup/Mappers/XmlBackupSetListMapper.cs
+++ QDBackup/Mappers/XmlBackupSetListMapper.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace QDBackup.Mappers
 {
-    class XmlBackupSetListMapper
+    public class XmlBackupSetListMapper : IBackupSetListMapper
     {
+        public string FilePath { get; protected set; }
+        protected XmlSerializer _serializer;
+
+        public XmlBackupSetListMapper(string path)
+        {
+            FilePath = path;
+            _serializer = new XmlSerializer(typeof(BackupSetList));
+        }
+
+        public BackupSetList Load()
+        {
+            if(File.Exists(FilePath))
+            {
+                return new BackupSetList(this);
+            }
+            else
+            {
+                var fileStream = new FileStream(FilePath, FileMode.Open);
+                return (BackupSetList)_serializer.Deserialize(fileStream);
+            }
+        }
+
+        public bool Save(BackupSetList list)
+        {
+            try
+            {
+                var fileStream = new FileStream(FilePath, FileMode.Create);
+                _serializer.Serialize(fileStream, list);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
     }
 }
