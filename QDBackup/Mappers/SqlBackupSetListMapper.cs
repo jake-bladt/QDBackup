5247002c4429ca60728299a54b350d73dcedcb8b
Jake Bladt <jake@jakebladt.com>
2014-03-16 11:21:12 -0500

    xml -> sql

---
+++ QDBackup/Mappers/SqlBackupSetListMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDBackup.Mappers
+{
+    public class SqlBackupSetListMapper : IBackupSetListMapper
+    {
+
+        protected string _ConnectionString;
+
+        public SqlBackupSetListMapper(string cnStr)
+        {
+            _ConnectionString = cnStr;
+        }
+
+        public BackupSetList Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(BackupSetList list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
